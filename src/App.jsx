d051f2692444f40ebe18e742e35fb4913b23808b
caitlinanderson099 <caitlin.anderson099@gmail.com>
2024-07-26 09:20:00 +1200

    added title

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import Albums from './Albums'
 import Header from './Header'
 import Footer from './Footer'
+import Home from './Home'
 
 const App = () => {
 
   return (
     <>
       <Header/>
+      <Home/>
       <Albums/>
       <Footer/>
     </>
---
+++ src/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+        <div id='title-text'>
+        <h1> These are my favourite albums and singles </h1>
+        <h2> Have a look! </h2>
+        </div>
+     
+    </>
+  )
+}
+
+export default Home
